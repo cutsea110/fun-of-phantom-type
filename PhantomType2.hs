d03aabd654d75d58f336bb42e9f549d6f5c20b79
cutsea110 <cutsea110@gmail.com>
2012-08-08 08:16:06 +0900

    format as generic function

--- PhantomType2.hs
+++ PhantomType2.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GADTs, ExistentialQuantification #-}
+{-# LANGUAGE OverloadedStrings #-}
 module PhantomType2 where
 
 s = \x y z -> (x z) (y z)
@@ -30,7 +31,31 @@ reflect :: forall t. Type t -> (Term t -> t)
 reflect (RBase) e = In e
 reflect (ra :-> rb) e = \x -> reflect rb (App e (reify ra x))
 
-
 b :: Type Base
 b = RBase
 
+data Btree a = Leaf a
+             | Fork (Btree a) (Btree a)
+             deriving (Show)
+
+flatten :: forall a. Btree a -> [a]
+flatten t = flatcat t []
+
+flatcat :: forall a. Btree a -> [a] -> [a]
+flatcat (Leaf a) as = a:as
+flatcat (Fork tl tr) as = flatcat tl (flatcat tr as)
+
+data Dir t p where
+  Lit :: String -> Dir t t
+  Int :: Dir t (Int -> t)
+  String :: Dir t (String -> t)
+  (:^:) :: Dir p1 p2 -> Dir t p1 -> Dir t p2
+
+format' :: forall t p. Dir t p -> (String -> t) -> (String -> p)
+format' (Lit s) = \cont out -> cont (out ++ s)
+format' (Int) = \cont out -> \i -> cont (out ++ show i)
+format' (String) = \cont out -> \s -> cont (out ++ s)
+format' (d1 :^: d2) = \cont out -> format' d1 (format' d2 cont) out
+
+format :: forall p. Dir String p -> p
+format d = format' d id ""
